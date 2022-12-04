22b0dfa6aea14f4804e5a595314d6238c9960245
fasteddie830 <montewar8@hotmail.com>
2022-12-04 00:20:16 +0000

    all this was missing from when i went to uni, for f....

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,32 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import './App.css';
 import Dashboard from '../Dashboard/Dashboard'
 import Preferences from '../Preferences/Preferences'
+import Login from "../Login/Login";
+import useToken from './useToken';
 
-function App(){
+
+function App() {
+    const {token, setToken} = useToken();
+
+    if (!token) {
+        return <Login setToken={setToken}></Login>
+    }
     return (
-        <></>
-    )
-}
+        <>
+            <div className="wrapper">
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<h1>Application</h1>} />
+                        <Route path="/dashboard" element={<Dashboard />} />
+                        <Route path="/preferences" element={<Preferences />} />
+                    </Routes>
+                </BrowserRouter>
+            </div>
+        </>
+    );
+};
+
+export default App;
